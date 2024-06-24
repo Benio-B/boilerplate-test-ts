f47559a6d97d75e67bdd33622722460b87e33994
Benjamin Bouillot <benjamin.bouillot@indy.fr>
2024-06-24 08:55:15 +0200

    Improve eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,9 @@ import antfu from '@antfu/eslint-config';
 export default antfu({
   stylistic: {
     semi: true,
+    overrides: {
+      'max-len': ['error', { code: 120 }],
+    },
   },
+  ignores: ['stryker.config.mjs'],
 });
